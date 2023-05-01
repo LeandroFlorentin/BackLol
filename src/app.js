3ea329f7878ebcb5ae7a5b3719ecf752c2bdc67a
unknown <leandro.florentin@hotmail.com>
2023-05-01 12:16:07 -0300

    *

--- src/app.js
+++ src/app.js
@@ -5,11 +5,9 @@ const routes = require('./routes/index.js')
 
 const servidor = express()
 
-const list = ["https://front-lol.vercel.app/"]
-
 servidor.use(morgan('dev'))
 servidor.use(cors({
-  origin: list
+  origin: "*"
 }))
 servidor.use('/', routes)
 
