0a5a6020b91e0497e7d1eec18dd17dc134437f52
unknown <leandro.florentin@hotmail.com>
2023-05-01 10:24:24 -0300

    cors

--- src/app.js
+++ src/app.js
@@ -6,7 +6,12 @@ const routes = require('./routes/index.js')
 const servidor = express()
 
 servidor.use(morgan('dev'))
-servidor.use(cors())
+servidor.use(cors({
+  origin: "https://front-lol.vercel.app/",
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  preflightContinue: false,
+  optionsSuccessStatus: 204
+}))
 servidor.use('/', routes)
 
 module.exports = servidor
